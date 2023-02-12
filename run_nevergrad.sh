b2cfb53b2e9ac1966a327f1bc11ad8fbb8687e57
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-12 15:39:41 -0500

    Try a much smaller game batch size

--- run_nevergrad.sh
+++ run_nevergrad.sh
@@ -5,8 +5,8 @@ mpiexec -np 12 python3 \
   --cutechess cutechess-cli \
   --stockfish stockfish \
   --book UHO_XXL_+0.90_+1.19.epd \
-  --tc 1.0+0.01 \
-  --games_per_batch 20000 \
+  --tc 10+0.1 \
+  --games_per_batch 96 \
   --cutechess_concurrency 8 \
   --evaluation_concurrency 3 \
   --ng_evals 100
