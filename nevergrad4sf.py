903656eb8978253a670c864681e4da910d79d87c
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-12 21:12:54 -0500

    Try different formatting for stats

--- nevergrad4sf.py
+++ nevergrad4sf.py
@@ -223,21 +223,22 @@ def ng4sf(
         print(f"   games/s               : %8.3f"
             % (batch.total_games * evals_done / used_time.total_seconds())
         )
-        pprint(var2int(**x.kwargs))
+        print(var2int(**x.kwargs))
         print(f"   score                 : %8.3f +- %8.3f"
             % (stats["score"] * 100, stats["score_error"] * 100)
         )
         print(f'   Elo                   : {stats["Elo"]:8.3f} +- {stats["Elo_error"]}')
-        print(f'   ldw                   : {stats["ldw_los"]:.3f} LOS - {stats["ldw"]}')
-        print(f'   pentanomial           : {stats["pentanomial_los"]:.3f} LOS - {stats["pentanomial"]}')
-        print("SPRT")
-        print("   LLR [u,l]              : {:.2f} {} [{:.2f},{:.2f}]".format(
+        print(f'   ldw                   : {stats["ldw"]}')
+        print(f'                           {stats["ldw_los"]:.2f}% LOS}')
+        print(f'   pentanomial           : {stats["pentanomial"]}')
+        print(f'                           {stats["pentanomial_los"]:.2f}% LOS')
+        print("   SPRT LLR [u,l]        : {:.2f} {} [{:.2f},{:.2f}]".format(
             a["LLR"], "(clamped)" if a["clamped"] else "", a["a"], a["b"]
         ))
-        print("   Elo                    :  {:.2f}".format(a["elo"]))
-        print("   Confidence interval    :  [{:.2f},{:.2f}] (95%)".format(a["ci"][0], a["ci"][1]))
-        print(f'   LOS                    :  {a["LOS"]:4.2%}')
-        print(f"   loss                   : {loss}")
+        print("   Elo                   : {:.2f}".format(a["elo"]))
+        print("   Confidence interval   : [{:.2f},{:.2f}] (95%)".format(a["ci"][0], a["ci"][1]))
+        print(f'   LOS                   : {a["LOS"]:4.2%}')
+        print(f"   loss                  : {loss}")
         print()
 
         # make a backup of the old restart and dump current state
