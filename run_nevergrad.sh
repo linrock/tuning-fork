aa2c00a048acfdc5143889a598285f2e1462c46a
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-12 22:00:14 -0500

    More formatting and more games per batch

--- run_nevergrad.sh
+++ run_nevergrad.sh
@@ -3,7 +3,7 @@
 mpiexec -np 8 python3 \
   -m mpi4py.futures nevergrad4sf.py \
   --tc "10000+10000 nodes=5000" \
-  --games_per_batch 192 \
+  --games_per_batch 256 \
   --cutechess_concurrency 8 \
   --evaluation_concurrency 3 \
-  --ng_evals 128
+  --ng_evals 256
