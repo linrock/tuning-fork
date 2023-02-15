2711281d1ec2a418797139055b374adac7dbc1f4
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-14 20:34:56 -0500

    Default of 64 batch size increase per iteration

--- run_nevergrad.sh
+++ run_nevergrad.sh
@@ -4,6 +4,7 @@ mpiexec -np 8 python3 \
   -m mpi4py.futures nevergrad4sf.py \
   --tc "10000+10000 nodes=5000" \
   --games_per_batch 256 \
+  --batch_increase_per_iter 64 \
   --cutechess_concurrency 8 \
   --evaluation_concurrency 3 \
   --ng_evals 256
