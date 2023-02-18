7a808ad1cf4a3319129210c691719621500f3ee2
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-17 23:48:52 -0500

    Show sshd process after setting up ssh

--- worker_ssh_setup.sh
+++ worker_ssh_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt install openssh-server
+apt install -y openssh-server
 sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
 mkdir /run/sshd
 /usr/sbin/sshd
+ps aux | grep sshd
