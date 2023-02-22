94d0505782568785f5b21e1fcf768f138f1ce061
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-22 16:59:28 -0500

    Lower default batch size to 96

--- run_nevergrad.sh
+++ run_nevergrad.sh
@@ -4,8 +4,8 @@ mpiexec -np 8 python3 \
   -m mpi4py.futures nevergrad4sf.py \
   --output_dir ./ng-tuning \
   --tc "10000+10000 nodes=5000" \
-  --games_per_batch 256 \
+  --games_per_batch 96 \
   --batch_increase_per_iter 64 \
   --cutechess_concurrency 8 \
   --evaluation_concurrency 3 \
-  --ng_evals 256
+  --ng_evals 512
--- stats/test.sh
+++ stats/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# source: https://github.com/glinscott/fishtest/tree/master/server/fishtest/stats
 python3 sprt.py --elo1 2.0 --elo-model normalized --results 6 28 54 33 11
